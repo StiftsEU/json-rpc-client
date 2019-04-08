3e5537a7b82cb6b8e0060153673053ae447386fc
Alexander Kozlenko <alexander.kozlenko@live.com>
2019-04-08 22:40:26 +0300

    Refactor source code files

--- src/Anemonis.JsonRpc.ServiceClient.UnitTests/JsonRpcClientTests.cs
+++ src/Anemonis.JsonRpc.ServiceClient.UnitTests/JsonRpcClientTests.cs
@@ -1,6 +1,4 @@
 using System;
-using System.IO;
-using System.IO.Compression;
 using System.Net.Http;
 using Anemonis.JsonRpc.ServiceClient.UnitTests.TestStubs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -262,32 +260,6 @@ namespace Anemonis.JsonRpc.ServiceClient.UnitTests
             using (var client = new TestJsonRpcClient())
             {
                 Assert.IsNotNull(client.PublicContractResolver);
-            }
-        }
-
-        private static class CompressionEncoder
-        {
-            public static byte[] Encode(byte[] content, string algorithm)
-            {
-                switch (algorithm)
-                {
-                    case "br":
-                        {
-                            using (var contentStream = new MemoryStream())
-                            {
-                                using (var compressionStream = new BrotliStream(contentStream, CompressionLevel.Optimal))
-                                {
-                                    compressionStream.Write(content, 0, content.Length);
-                                }
-
-                                return contentStream.ToArray();
-                            }
-                        }
-                    default:
-                        {
-                            throw new NotSupportedException();
-                        }
-                }
             }
         }
     }
